44d3147db5577fcafc95b3d07d26644b508ac731
Administrator <aa.163.com>
2019-12-29 20:23:34 +0800

    1.0.1

--- test/src/main/java/com/youlexuan/Test.java
+++ test/src/main/java/com/youlexuan/Test.java
@@ -2,6 +2,11 @@ package com.youlexuan;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("厂长憨批");
 
+    }
+
+    public void test(){
+        System.out.println("刘阿龙是憨批");
     }
 }
